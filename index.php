2d5d74affc2627f2a5433b05abb06e606c03e83e
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-11 09:13:59 -0300

    Funções com textos

--- index.php
+++ index.php
@@ -79,22 +79,63 @@ echo "</br>";
 $lista = array("Fulano", "André", "Maria", "Lucas");
 $x = rand(0,3);
 echo "Sorteado: ".$lista[$x];
+echo "</br>";
 
 //Manipulação de texto
 
+//Explode 
+$nome1 = "Deusyvan Silva";
+$x = explode(" ", $nome1);
+print_r($x);
+$nome2 = "Deusyvan Ferreira da Silva";
+echo "</br>";
+$y = explode("da", $nome2);
+print_r($y);
+echo "</br>";
 
+//Implode
+$array = array("Deusyvan", "Ferreira", "da", "Silva");
+$nomecompleto = implode(" ", $array);
+echo $nomecompleto;
+echo "</br>";
 
+//Number Format
+$z = number_format(8.5455588822,2);
+echo $z;
+echo "</br>";
+echo number_format(3458798.124587,2);
+echo "</br>";
+echo number_format(3458798.124587,2,",",".");
+echo "</br>";
+echo number_format(3458798.124587,2,",","");
+echo "</br>";
 
+//String replace
+$texto = "O rato roeu a roupa!";
+$string = str_replace("roeu", "comeu",$texto);
+echo $string;
+echo "</br>";
 
+//Maiúsculas em minúsculas
+echo strtolower("DEUSYVAN FERREIRA DA SILVA");
+echo "</br>";
 
+//minúsculas em Maiúsculas 
+echo strtoupper("maria eduarda bomfim");
+echo "</br>";
 
+//Substring
+echo substr("Deusyvan Ferreira da Silva", 0, 3);
+echo "</br>";
+echo substr("Deusyvan Ferreira da Silva", 1, 3);
+echo "</br>";
+echo substr("Deusyvan Ferreira da Silva", 2, 3);
+echo "</br>";
 
-
-
-
-
-
-
+//ucfirst Primeira maiúscula
+echo ucfirst("deusyvan ferreira silva");
+echo "</br>";
+echo ucwords("deusyvan ferreira da silva");
 
 
 
